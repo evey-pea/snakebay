7b66446973cdf4448ec445c993064ae15f80697a
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-04-30 14:07:01 +1000

    Updated repo

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -71,7 +71,7 @@ class ListingsController < ApplicationController
     end
 
     def listing_params
-        params.require(:listing).permit(:title, :description, :breed_id, :sex, :city, :state, :price, :deposit, :date_of_birth, :diet, :picture)
+        params.require(:listing).permit(:title, :description, :breed_id, :sex, :city, :state, :price, :deposit, :date_of_birth, :diet, :picture, :user )
     end
 
 end
